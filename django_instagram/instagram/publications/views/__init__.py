506b7aa488d6a56dec81fbc6ffebe0d0b3db04de
nnnocturn <vikchehovich@gmail.com>
2021-02-03 02:33:06 +0200

    delete

--- django_instagram/instagram/publications/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .post_views import PostListView, PostDeleteView, PostCreateView, PostEditView, PostDetailView
-from .comment_views import CommentCreateView, CommentDeleteView
-from .like_views import LikeView
--- django_instagram/instagram/users/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user_views import UserDetailView, UserEditView, UserPasswordChangeView, UserLogoutView, account_login_view, \
-    account_register_view
-from .subscription_views import UserSubscribeView
