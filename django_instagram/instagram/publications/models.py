518efc0c8e86a80786192c8808b4e12cef0216da
nnnocturn <vikchehovich@gmail.com>
2021-02-03 02:26:37 +0200

    all

---
+++ django_instagram/instagram/publications/models.py
@@ -0,0 +1,97 @@
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        'users.User',
+        related_name='posts',
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        null=False,
+        blank=False,
+        default=None
+    )
+    image = models.ImageField(
+        null=False,
+        blank=False,
+        upload_to='post_pics',
+        verbose_name='Картинка'
+    )
+    text = models.TextField(
+        max_length=500,
+        verbose_name='Текст',
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        text = ' '.join(self.text.split(' ')[:4])
+        return f'{self.author.username} - {text}...'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        'users.User',
+        related_name='comments',
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        null=False,
+        blank=False,
+        default=None
+    )
+    post = models.ForeignKey(
+        'publications.Post',
+        related_name='comments',
+        on_delete=models.CASCADE,
+        verbose_name='Пост',
+        null=False,
+        blank=False,
+        default=None
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+        null=False,
+        blank=False,
+        default=None,
+        max_length=100
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        text = ' '.join(self.text.split(' ')[:4])
+        return f'{self.post.pk} - {self.author.username} - {text}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        'users.User',
+        related_name='likes',
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        null=False,
+        blank=False,
+        default=None
+    )
+    post = models.ForeignKey(
+        'publications.Post',
+        related_name='likes',
+        on_delete=models.CASCADE,
+        verbose_name='Пост',
+        null=False,
+        blank=False,
+        default=None
+    )
+
+    def __str__(self):
+        return f'{self.user.username} понравился пост {self.post}'
---
+++ django_instagram/instagram/users/views/__init__.py
@@ -0,0 +1,3 @@
+from .user_views import UserDetailView, UserEditView, UserPasswordChangeView, UserLogoutView, account_login_view, \
+    account_register_view
+from .subscription_views import UserSubscribeView
